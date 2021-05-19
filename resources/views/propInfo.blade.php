ce3bb14ce620f4203c9d1a9e82ab7bfed24be71e
fc53293 <gongas.figueiredo09@gmail.com>
2021-05-19 21:45:13 +0100

    commit teste

--- resources/views/propInfo.blade.php
+++ resources/views/propInfo.blade.php
@@ -26,7 +26,7 @@
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
     </script>
     <script src="/js/scripts.js"></script>
-    <script src="/js/mapsAPI.js"></script>
+    <script src="/js/mapsAPI.js"></script> 
 </head>
 
 <body>
